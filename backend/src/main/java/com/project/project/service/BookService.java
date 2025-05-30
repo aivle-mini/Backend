97c7cba57dae27b0159867305dda7be782652944
leeyumin <lee_yumin@naver.com>
2025-05-30 16:08:31 +0900

    book interface 추가

--- backend/src/main/java/com/project/project/service/BookService.java
+++ backend/src/main/java/com/project/project/service/BookService.java
@@ -15,60 +15,22 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-@Transactional
-public class BookService {
-    private final BookRepository bookRepository;
-    private final UserRepository userRepository;
-    private final BookControlMapper mapper;
-    private final BookResponseMapper responseMapper;
-
+public interface BookService {
     // 모든 도서 조회 yml
-    public List<BookDTO.Response> findBooks() {
-        List<Book> books = bookRepository.findAll();
-        System.out.println(books);
-        System.out.println(responseMapper.booksToResponses(books));
-        return responseMapper.booksToResponses(books);
-    }
+    public List<BookDTO.Response> findBooks();
 
     // 특정 도서 조회 yml
-    public BookDTO.Response findBook(Long id) {
-        Book book = findVerifiredBook(id);
-        return responseMapper.entityToResponse(book);
-    }
+    public BookDTO.Response findBook(Long id);
 
     // 도서 삭제 yml
-    public BookDTO.Response deleteBook(Long id) {
-        Book book = findVerifiredBook(id);
-        boolean deleted = Boolean.TRUE.equals(book.getIs_deleted());
-
-        book.setIs_deleted(!deleted); // 토글
-        return responseMapper.entityToResponse(book);
-    }
+    public BookDTO.Response deleteBook(Long id);
 
     // 데이터 유무 검증 yml
-    public Book findVerifiredBook(Long id) {
-        return bookRepository.findById(id)
-                .orElseThrow(() -> BaseException.type(BookErrorCode.NOT_FOUND_BOOK));
-    }
+    public Book findVerifiredBook(Long id);
 
     //추가로직
-    public Book createBook(BookDTO.Post dto) {
-        User user = userRepository.findById(dto.getUserId())
-                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
-        Book book = mapper.toEntity(dto);
-        book.setUser(user);
-        return bookRepository.save(book);
-    }
+    public Book createBook(BookDTO.Post dto);
 
     //수정로직
-    public Book updateBook(Long id, BookDTO.Put dto) {
-        Book book = bookRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("책을 찾을 수 없습니다."));
-        book.setTitle(dto.getTitle());
-        book.setContent(dto.getContent());
-        book.setImage_url(dto.getImage_url());
-        return bookRepository.save(book);
-    }
+    public Book updateBook(Long id, BookDTO.Put dto);
 }
